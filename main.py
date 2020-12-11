09647573d3dd2db28082a7e4e1232bc5880a651d
Alan do Nascimento <alandicaprio44@gmail.com>
2020-12-11 17:22:47 +0000

    está rodando ok.

--- main.py
+++ main.py
@@ -4,6 +4,10 @@
 def main():
     """Função principal que será rodada quando o script for passado para o interpretador."""
     # COLOQUE SEU CÓDIGO AQUI
+    peso = float(input('Digite o peso total dos peixes: '))
+    exc = max(0, peso-50)
+    multa = exc * 4
+    print(f'O peixe pescado tem {peso}kg, a multa devida é R$ {multa:.2f}.')
 
 
 if __name__ == '__main__':
